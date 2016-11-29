77e4907ce70e46caf678047c9bf73829911b434e
itn3000 <itn3000@gmail.com>
2016-11-29 12:58:14 +0900

    add commands

--- src/extension.ts
+++ src/extension.ts
@@ -7,23 +7,74 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "open-nuget" is now active!');
-
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    registerOpenSearchCommand(context)
+    registerExactPackageCommand(context)
+}
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+function registerOpenSearchCommand(context: vscode.ExtensionContext)
+{
+    let disposable = vscode.commands.registerCommand('extension.openNuGetPackageSearch', () => {
+        vscode.window.showInputBox({
+            prompt: "input words for searching nuget package"
+        })
+        .then(value => {
+            if(value == null || value == "")
+            {
+                // return vscode.window.showInformationMessage("open cancelled")
+                return
+            }
+            let uri = vscode.Uri.parse(
+                "https://www.nuget.org/packages?q=" 
+                    + encodeURI(value.replace(" ","+").replace("\t","+"))
+                )
+            return vscode.commands.executeCommand('vscode.open',uri)
+                .then(value => {
+                    vscode.window.showInformationMessage("open success")
+                })
+        })
+        .then(value => {
+        },
+        reason => {
+            vscode.window.showErrorMessage("open failed:" + reason)
+        })
     });
 
     context.subscriptions.push(disposable);
 }
 
+function registerExactPackageCommand(context: vscode.ExtensionContext)
+{
+    let disposable = vscode.commands.registerCommand('extension.openExactNuGetPackage', () => {
+        vscode.window.showInputBox({
+            prompt: "input nuget package id"
+        })
+        .then(value => {
+            if(value == null || value == "")
+            {
+                // return vscode.window.showInformationMessage("open cancelled")
+                return
+            }
+            let uri = vscode.Uri.parse(
+                "https://www.nuget.org/packages/" 
+                    + encodeURI(value)
+                )
+            return vscode.commands.executeCommand('vscode.open',uri)
+                .then(value => {
+                    vscode.window.showInformationMessage("open success")
+                })
+        })
+        .then(value => {
+        },
+        reason => {
+            vscode.window.showErrorMessage("open failed:" + reason)
+        })
+    });
+
+    context.subscriptions.push(disposable);
+}
 // this method is called when your extension is deactivated
 export function deactivate() {
 }
